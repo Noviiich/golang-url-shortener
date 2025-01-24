d4e39836a6f4a507b5263894c28acb7dc29324dd
Noviiich <nowikoff63@gmail.com>
2025-01-24 21:31:03 +0300

    fix: change link for mongoDB

--- types/link.go
+++ types/link.go
@@ -3,8 +3,8 @@ package types
 import "time"
 
 type Link struct {
-	Id       string    `json:"id"`
-	Short    string    `json:"short"`
-	Long     float64   `json:"long"`
-	CreateAt time.Time `json:"create_at"`
+	Id       string    `bson:"id" json:"id"`
+	Short    string    `bson:"short" json:"short"`
+	Long     float64   `bson:"long" json:"long"`
+	CreateAt time.Time `bson:"create_at" json:"create_at"`
 }
