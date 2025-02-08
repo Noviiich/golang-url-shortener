6c3ada4793047a46ab5f6cf5aa3297121f39d2c9
Noviiich <nowikoff63@gmail.com>
2025-02-08 14:26:14 +0300

    add fillCache

--- internal/adapters/functions/redirect/main_test.go
+++ internal/adapters/functions/redirect/main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/Noviiich/golang-url-shortener/internal/adapters/handler"
 	"github.com/Noviiich/golang-url-shortener/internal/adapters/mock"
+	"github.com/Noviiich/golang-url-shortener/internal/core/model"
 	"github.com/Noviiich/golang-url-shortener/internal/core/service"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
@@ -16,6 +18,7 @@ func setupTest(shortID string) (*httptest.ResponseRecorder, error) {
 	gin.SetMode(gin.ReleaseMode)
 	mockDB := mock.NewMockRepository()
 	cache := mock.NewMockRedisCache()
+	fillCache(cache, mockDB.Links)
 	service := service.NewURLService(mockDB, cache)
 	handler := handler.NewURLHandler(service)
 
@@ -43,4 +46,14 @@ func TestRedirectShortURL_NotFound(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, http.StatusNotFound, response.Code)
+}
+
+func fillCache(cache *mock.MockRedisCache, links map[string]model.Link) error {
+	for _, link := range links {
+		_, err := cache.Set(context.Background(), link.ShortID, link.OriginalURL)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
