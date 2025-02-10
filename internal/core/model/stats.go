ff21f2ae15693a04bca4b2649c7dd7afd2bd5b99
Noviiich <nowikoff63@gmail.com>
2025-02-10 19:16:00 +0300

    add stats

---
+++ internal/core/model/stats.go
@@ -0,0 +1,32 @@
+package model
+
+import "time"
+
+type Platform int
+
+const (
+	PlatformUnknown Platform = iota
+	PlatformInstagram
+	PlatformTwitter
+	PlatformYouTube
+)
+
+func (p Platform) String() string {
+	switch p {
+	case PlatformInstagram:
+		return "Instagram"
+	case PlatformTwitter:
+		return "Twitter"
+	case PlatformYouTube:
+		return "YouTube"
+	default:
+		return "Unknown"
+	}
+}
+
+type Stats struct {
+	Id        string    `json:"id" bson:"id"`
+	Platform  Platform  `json:"platform" bson:"platform"`
+	LinkID    string    `json:"link_id" bson:"link_id"`
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+}
